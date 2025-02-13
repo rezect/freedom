87112927fbafbc86149791fc562d98a5fe39eb5b
rezect <ilyapr1568@yandex.ru>
2025-02-13 18:45:42 +0300

    forward list realisation

---
+++ stl/list/forward/exceptions.hpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class ListIsEmptyException : std::exception {
+public:
+  explicit ListIsEmptyException(const std::string &text)
+      : error_message_(text) {}
+
+  const char *what() const noexcept override { return error_message_.data(); }
+
+private:
+  std::string_view error_message_;
+};
